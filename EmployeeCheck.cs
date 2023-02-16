83fb9e7d76bc70dbcea7893065af2ddcb2b02007
Nikhlesh Rao <38323670+Nikhlesh117@users.noreply.github.com>
2023-02-16 09:25:23 +0530

    Added:  Calculating Wages till conditions are meet

---
+++ EmployeeCheck.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    public class EmployeeCheck
+    {
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int NO_OF_WORKING_DAYS = 20;
+        public const int MAX_HRS_IN_MONTH = 100;
+        public void CheckEmployee()
+        {
+            int empHrs = 0;
+            int empWage = 0;
+            int totalEmpWage = 0;
+            int totalWorkingDays = 0;
+            int totalEmpHrs = 0;
+
+            while (totalWorkingDays < NO_OF_WORKING_DAYS && totalEmpHrs < MAX_HRS_IN_MONTH)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(3);
+
+                switch (empCheck)
+                {
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        Console.WriteLine("Employee is Absent");
+                        break;
+                }
+
+                totalEmpHrs += empHrs;
+                empWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+                totalEmpWage += empWage;
+
+                Console.WriteLine("Day #: " + totalWorkingDays + " Emp Hrs : " + empHrs);
+            }
+            Console.WriteLine("Total Employee wage: " + totalEmpWage);
+        }
+    }
+}
