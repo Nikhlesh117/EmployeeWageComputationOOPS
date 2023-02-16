da835ed7afd9dc5e3992e97b0b87cff6747d06b9
Nikhlesh Rao <38323670+Nikhlesh117@users.noreply.github.com>
2023-02-16 08:37:00 +0530

    Added: Checking employee is present or absent

---
+++ EmployeeCheck.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    public class EmployeeCheck
+    {
+        public int IS_FULLTIME = 1;
+        public void CheckEmployee()
+        {
+            Random random = new Random();
+            int empCheck = random.Next(2);
+
+            if (empCheck == IS_FULLTIME)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+    }
+}
