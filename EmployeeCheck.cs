2f3cd6e897a9c23f19edc56fc53707e4e4097cbb
Nikhlesh Rao <38323670+Nikhlesh117@users.noreply.github.com>
2023-02-16 08:45:02 +0530

    Added: Calculate Employee Wage

---
+++ EmployeeCheck.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    public class EmployeeCheck
+    {
+        public int IS_FULLTIME = 1;
+        public int EMP_RATE_PER_HOUR = 20;
+        public void CheckEmployee()
+        {
+            int empHrs = 0;
+            int empWage = 0;
+
+            Random random = new Random();
+            int empCheck = random.Next(2);
+
+            if (empCheck == IS_FULLTIME)
+            {
+                empHrs = 8;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+
+            Console.WriteLine("Employee wage is: " + empWage);
+        }
+    }
+}
