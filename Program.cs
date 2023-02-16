d88d12ea491265a28b3ad0043ec8eababac9d8ff
Nikhlesh Rao <38323670+Nikhlesh117@users.noreply.github.com>
2023-02-16 09:19:11 +0530

    Added: Calling UC5

--- Program.cs
+++ Program.cs
@@ -5,6 +5,13 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Employee Wage computation problem");
+
+
+            EmployeeCheck WagesMonth = new EmployeeCheck();
+            WagesMonth.CheckEmployee();
+
+            
+
         }
     }
 }
