7d47c88711fe2062a4899f01db82056909cc8a7d
Nikhlesh Rao <38323670+Nikhlesh117@users.noreply.github.com>
2023-02-16 09:04:15 +0530

    Added: Solving using Switch Case Statement

---
+++ EmployeeCheck.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    public class EmployeeCheck
+    {
+        public int IS_FULL_TIME = 1;
+        public int IS_PART_TIME = 2;
+        public int EMP_RATE_PER_HOUR = 20;
+        public void CheckEmployee()
+        {
+            int empHrs = 0;
+            int empWage = 0;
+
+            Random random = new Random();
+            int empCheck = random.Next(3);
+
+            switch (empCheck)
+            {
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+                    Console.WriteLine("Employee is Absent");
+                    break;
+            }
+
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+
+            Console.WriteLine("Employee wage is: " + empWage);
+        }
+    }
+}
