3cca604fb544304d918dbd36fc369a0f73afa31a
Nikhlesh Rao <38323670+Nikhlesh117@users.noreply.github.com>
2023-02-18 13:09:07 +0530

    Added: Calling UC8

--- Program.cs
+++ Program.cs
@@ -5,6 +5,14 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Employee Wage computation problem");
+
+
+            ComputeWage MultipleCompany = new ComputeWage();
+            MultipleCompany.calculateWage("TaTa", 20, 20, 160);
+            MultipleCompany.calculateWage("Jindal", 10, 20, 120);
+
+
+
         }
     }
 }
