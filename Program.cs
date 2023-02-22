f428cea88701dfb5c830080c1c02c02be6197221
Nikhlesh Rao <38323670+Nikhlesh117@users.noreply.github.com>
2023-02-22 11:49:38 +0530

    Added: Calling UC9

--- Program.cs
+++ Program.cs
@@ -5,6 +5,19 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Employee Wage computation problem");
+
+
+            ComputeWage obj = new ComputeWage("TaTa", 20, 20, 160);
+            obj.computeEmpWage();
+            Console.WriteLine(obj.toString());
+
+            ComputeWage obj1 = new ComputeWage("Jindal", 10, 20, 120);
+            obj1.computeEmpWage();
+            Console.WriteLine(obj1.toString());
+
+
+
+
         }
     }
 }
