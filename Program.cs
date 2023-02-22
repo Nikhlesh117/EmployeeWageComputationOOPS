b24866dc67555acc42437e09d3003fd35ed847fc
Nikhlesh Rao <38323670+Nikhlesh117@users.noreply.github.com>
2023-02-22 12:09:47 +0530

    Added: Calling UC10

--- Program.cs
+++ Program.cs
@@ -5,6 +5,16 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Employee Wage computation problem");
+
+
+            ComputeWage ManageCompany = new ComputeWage();
+
+            ManageCompany.addCompanyEmpWage("TaTa", 20, 20, 160);
+            ManageCompany.addCompanyEmpWage("Jindal", 10, 20, 120);
+            ManageCompany.computeEmpWage();
+
+
+
         }
     }
 }
