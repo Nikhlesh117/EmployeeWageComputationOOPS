888c3d82102ad353e65b3073a57af1363d9a2f3f
Nikhlesh Rao <38323670+Nikhlesh117@users.noreply.github.com>
2023-02-22 12:29:13 +0530

    Added: Calling UC11

--- Program.cs
+++ Program.cs
@@ -5,6 +5,16 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Employee Wage computation problem");
+
+
+            IComputeEmgWage ManageCompanyInterface = new ComputeWage();
+
+            ManageCompanyInterface.addCompanyEmpWage("TaTa", 20, 20, 160);
+            ManageCompanyInterface.addCompanyEmpWage("Jindal", 10, 20, 120);
+            ManageCompanyInterface.computeEmpWage();
+
+
+
         }
     }
 }
