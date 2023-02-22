95019829298acd59e200a7b84c40b22ba5c0961e
Nikhlesh Rao <38323670+Nikhlesh117@users.noreply.github.com>
2023-02-22 13:30:25 +0530

    Added: Array List to Manage Multiple Companies

---
+++ IComputeEmgWage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    public interface IComputeEmgWage
+    {
+        public void addCompanyEmpWage(string company, int empRate, int workingDays, int maxHours);
+        public void computeEmpWage();    
+    }
+}
